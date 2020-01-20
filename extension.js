7fe077556c25b2333b5f372560467b95f2b84e6e
Sefer Ergün <46936258+seferergun@users.noreply.github.com>
2020-01-20 23:12:50 +0300

    Added createNewWorkspace function for creating a new workspace when windows is maximized

--- extension.js
+++ extension.js
@@ -1,41 +1,34 @@
 const Meta = imports.gi.Meta;
+const GLib = imports.gi.GLib;
 
-/* This has been tested with both static and dynamic workspaces.  It
- * works better with dynamic workspaces, but can work well if you have
- * enough static ones.
- */
 
-/* Possible future options: (OO)
- *  always move to last / find first empty / find last empty
- *  target a specific desktop when none are empty
- *  (don't) skip first desktop
- */
+const desktopTable = {};
+
+
+function createNewWorkspace() {
+  const workspaceManager = global.workspace_manager;
+  return workspaceManager.append_new_workspace(true, new GLib.DateTime().to_unix());
+}
+
 function check(act) {
   const win = act.meta_window;
-  const workspacemanager = win.get_display().get_workspace_manager();
   if (win.window_type !== Meta.WindowType.NORMAL)
     return;
   if (win.get_maximized() !== Meta.MaximizeFlags.BOTH)
     return;
-  w = win.get_workspace().list_windows()
-    .filter(w => w !== win && !w.is_always_on_all_workspaces());
-  if (w.length>= 1) {
-    // put on last workspace if all else fails (OO)
-    let lastworkspace = workspacemanager.get_n_workspaces()-1
-    // always start with the second workspace (OO)
-    if (lastworkspace<1) lastworkspace=1
-    let wc;
-    let emptyworkspace;
-    for (emptyworkspace=1 ; emptyworkspace<lastworkspace; emptyworkspace++){
-      wc = workspacemanager.get_workspace_by_index(emptyworkspace).list_windows().filter(w=>!w.is_always_on_all_workspaces()).length
-      if (wc<1	) break;
-    }
-    // don't try to move it if we're already here (break recursion)
-    if (emptyworkspace == win.get_workspace().index())
-      return;
-    win.change_workspace_by_index(emptyworkspace,1)
-    workspacemanager.get_workspace_by_index(emptyworkspace).activate(global.get_current_time())
+  
+  if (desktopTable[win]) {
+    return;
   }
+  const newWorkspace = createNewWorkspace();
+  win.change_workspace(newWorkspace);
+  desktopTable[win] = newWorkspace;
+  /*win.get_workspace().list_windows()
+    .filter(w => w !== win)
+    .reduce((isFirst, w) => {
+      w.change_workspace_by_index(win.get_workspace().index() + 1, isFirst);
+      return false;
+    }, true);*/
 }
 
 const _handles = [];
@@ -46,15 +39,18 @@ function enable() {
   _handles.push(global.window_manager.connect('size-change', (_, act, change) => {
     if (change === Meta.SizeChange.MAXIMIZE)
       check(act);
+      
   }));
-  _handles.push(global.window_manager.connect('switch-workspace', () => {
+  /*_handles.push(global.window_manager.connect('switch-workspace', () => {
     const acts = global.get_window_actors()
       .filter(a => a.meta_window.has_focus());
     if (acts.length)
       check(acts[0]);
-  }));
+  }));*/
+  
 }
 
 function disable() {
   _handles.splice(0).forEach(h => global.window_manager.disconnect(h));
 }
+
